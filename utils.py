0c80770731caba5be918b6fe54b4df7851b1b35e
borys25ol <oleynik.boris@gmail.com>
2021-01-05 11:46:49 +0200

    Save logs to file in prod mode

--- utils.py
+++ utils.py
@@ -8,15 +8,19 @@ def create_logger(logger_name):
     """
     Initialize a logger.
     """
+    config_name = os.getenv("CONFIG") or "dev"
+
     logger = logging.getLogger(logger_name)
 
     if not os.path.exists(config.LOGGER_PATH.parent):
         os.makedirs(config.LOGGER_PATH.parent, exist_ok=True)
 
-    logging.basicConfig(
-        format=config.LOGGER_FORMAT,
-        filename=config.LOGGER_PATH,
-    )
+    if config_name == "prod":
+        logging.basicConfig(
+            format=config.LOGGER_FORMAT, filename=config.LOGGER_PATH,
+        )
+    else:
+        logging.basicConfig(format=config.LOGGER_FORMAT)
 
     logger.setLevel(level=logging.INFO)
 
