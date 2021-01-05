7b71d46ce6f74547d4915f762abe1c734bd77d3b
borys25ol <oleynik.boris@gmail.com>
2021-01-05 11:50:07 +0200

    Add simple cache to app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
@@ -13,6 +14,8 @@ config_name = os.getenv("CONFIG") or "dev"
 
 app.config.from_object(config_by_name[config_name])
 
+cached_numbers = defaultdict(list)
+
 
 @app.route("/api/health-check")
 def heal_check():
@@ -22,18 +25,25 @@ def heal_check():
 @app.route("/api/roulette", methods=["POST"])
 @cross_origin()
 def add_number():
+    global cached_numbers
+
     content = request.json
 
     numbers, source = content["numbers"], content["source"]
-    service = GoogleSpreadsheetService(sheet_name=app.config["SHEET_NAME"][source])
 
-    added_numbers = service.append_data_row(values=numbers, column=source)
-    total_numbers = service.get_column_data(column=source)
+    if len(cached_numbers[source]) == 0:
+        cached_numbers[source] = numbers
 
-    return jsonify(
-        success=True, added_numbers=len(added_numbers), total_numbers=len(total_numbers)
-    )
+    if cached_numbers[source] != numbers:
+        app.logger.info("Changes in cache detected. Got new numbers.")
 
+        service = GoogleSpreadsheetService(sheet_name=app.config["SHEET_NAME"][source])
+        added_numbers = service.append_data_row(values=numbers, column=source)
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+        cached_numbers[source] = numbers
+
+        return jsonify(success=True, added_numbers=len(added_numbers))
+    else:
+        app.logger.info("Got no new numbers from the website.")
+
+        return jsonify(success=True, added_numbers=0)
