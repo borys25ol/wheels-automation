b84077f5bfa1eb24e7d3be10ae45fe8cf06318c1
borys25ol <oleynik.boris@gmail.com>
2021-01-06 18:16:34 +0200

    Add docstrings to base wheels manager.

--- wheels/_base.py
+++ wheels/_base.py
@@ -11,6 +11,10 @@ from utils import create_logger
 
 
 class BaseCasinoManager:
+    """
+    Base Manager class with general logic for wheels automation.
+    """
+
     def __init__(self, script_path, casino_url):
         self.driver = self.init_webdriver()
         self.script_path = script_path
@@ -18,8 +22,81 @@ class BaseCasinoManager:
 
         self.logger = create_logger(__name__)
 
+    def load_page(self, url):
+        """
+        Move to specific `url` in browser.
+        """
+        self.logger.info("Loading page: `%s`", url)
+        self.driver.get(url)
+
+    def login_confirm(self):
+        """
+        Blocking function for any manual
+        actions before start main logic.
+        """
+        input("Hit Enter here if you have summited the form: <Enter>")
+        self.logger.info("Login successfully approved!")
+
+    def switch_to_top_frame(self):
+        """
+        Switch iFrame to main screen.
+        """
+        self.driver.switch_to.default_content()
+
+    def reload_page(self):
+        """
+        Refresh current page.
+        It needs for session expiration management.
+        """
+        self.driver.refresh()
+
+    def run_casino(self):
+        raise NotImplemented()
+
+    def check_session_expired(self):
+        raise NotImplemented()
+
+    def execute_script(self):
+        raise NotImplemented()
+
+    def start_process(self):
+        """
+        Run infinite loop for checking session expiration.
+
+        If session expired this method should restart all logic
+        for scraping numbers:
+            - Reload the current page.
+            - Open casino, wait for page loaded and switch frames.
+            - Execute scraping JS script.
+        """
+        while True:
+            if self.check_session_expired():
+                self.logger.info("Session expired. Reload page!")
+                self.reload_page()
+
+                success = self.run_casino()
+
+                if not success:
+                    continue
+
+                self.execute_script()
+
+            self.logger.info("Sleep 10 sec before check session expired!")
+            time.sleep(10)
+
+    def close(self):
+        """
+        Close current tab in browser and stop driver.
+        """
+        self.driver.close()
+        self.driver.quit()
+
     @staticmethod
     def init_webdriver():
+        """
+        Initialize Chrome Webdriver for local using.
+        It uses `WebDriver Manager` for managing version of the browsers.
+        """
         options = webdriver.ChromeOptions()
         options.add_argument("--start-maximized")
         browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
@@ -28,6 +105,9 @@ class BaseCasinoManager:
 
     @staticmethod
     def init_remote_webdriver():
+        """
+        Initialize Remote Webdriver for using with Selenoid.
+        """
         chrome_options = webdriver.ChromeOptions()
         capabilities = {
             "acceptInsecureCerts": True,
@@ -43,45 +123,3 @@ class BaseCasinoManager:
             keep_alive=True,
         )
         return browser
-
-    def load_page(self, url):
-        self.logger.info("Loading page: `%s`", url)
-        self.driver.get(url)
-
-    def login_confirm(self):
-        input("Hit Enter here if you have summited the form: <Enter>")
-        self.logger.info("Login successfully approved!")
-
-    def run_casino(self):
-        raise NotImplemented()
-
-    def switch_to_top_frame(self):
-        self.driver.switch_to.default_content()
-
-    def reload_page(self):
-        self.driver.refresh()
-
-    def check_session_expired(self):
-        raise NotImplemented()
-
-    def execute_script(self):
-        raise NotImplemented()
-
-    def start_process(self):
-        while True:
-            if self.check_session_expired():
-                self.logger.info("Session expired. Reload page!")
-                self.reload_page()
-                success = self.run_casino()
-
-                if not success:
-                    continue
-
-                self.execute_script()
-
-            self.logger.info("Sleep 10 sec before check session expired!")
-            time.sleep(10)
-
-    def close(self):
-        self.driver.close()
-        self.driver.quit()
